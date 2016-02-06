3b6e77403159e64e804ad1869eec7ed2d1920997
Gordon <gordonz@umich.edu>
2016-02-06 15:03:59 -0500

    Added enemy assets and scripts moved enemytest scene to a single folder, updated .gitignore`

---
+++ Assets/__Scripts/EnemyBaseBehavior.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum EnemyState { normal = 0,  }
+
+public class EnemyBaseBehavior : MonoBehaviour {
+    //Properties
+    public int MaxHP = 10;
+    public float DefenseMult = 1.0f;
+    public float MovementSpeed = .5f;
+    public GameObject Weapon;
+
+    //enemy status
+    public bool __Status_____________;
+    public int currHP;
+    public 
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
