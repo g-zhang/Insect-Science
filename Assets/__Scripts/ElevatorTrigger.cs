47d8882a26d2cb591416bacceeaf15e74c6f1c02
goodingr <goodingr@umich.edu>
2016-02-08 17:09:32 -0500

    Stage 1, scientist, elevators

---
+++ Assets/__Scripts/ElevatorTrigger.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevatorTrigger : MonoBehaviour {
+
+    public ElevatorTrigger destination;
+    public bool active = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter(Collider other) {
+        ShowUI();
+
+    }
+    void OnTriggerStay(Collider other) {
+        float interact = Input.GetAxis("Interact");
+        if (interact > 0 && active) {
+            UseElevator(); 
+        }
+    }
+    // Shows the "Press e to use elevator" UI
+    public void ShowUI() {
+        
+    }
+    
+    public void UseElevator() {
+           Vector3 pos = Scientist.S.transform.position;
+           pos.y = destination.transform.position.y + 0.1f;
+           Scientist.S.transform.position = pos;
+           destination.active = false;
+           Invoke("ActivateDestinationElevator", 1);
+    }
+    
+    public void ActivateDestinationElevator() {
+        destination.active = true;
+    }
+    
+}
---
+++ Assets/__Scripts/Scientist.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scientist : MonoBehaviour {
+
+    public static Scientist S;
+
+
+	void Start () {
+	   S = this;
+	}
+	
+	
+}
