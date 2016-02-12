1b0bfad0e9338d4279795a508e0e42cee883393e
npcook <nicholas.cook+github@outlook.com>
2016-02-12 15:25:35 -0500

    Add hall doors and fly charges

--- Assets/__Scripts/Scientist.cs
+++ Assets/__Scripts/Scientist.cs
@@ -5,10 +5,23 @@ public class Scientist : MonoBehaviour {
 
     public static Scientist S;
 
+	bool usedDoor = false;
 
 	void Awake () {
 	   S = this;
 	}
-	
-	
+
+	void OnTriggerStay(Collider other) {
+		var door = other.GetComponentInParent<HallDoor>();
+		if (Input.GetKeyDown(KeyCode.Z) && door != null && !usedDoor) {
+			var diff = transform.position - other.transform.position;
+			transform.position = diff + door.linkedDoor.transform.position + new Vector3(0f, 0.1f, 0f);
+			usedDoor = true;
+			Invoke("ResetUsedDoor", 0.1f);
+		}
+	}
+
+	void ResetUsedDoor() {
+		usedDoor = false;
+	}
 }
