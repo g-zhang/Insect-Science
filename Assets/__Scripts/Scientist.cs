ee3d1e05021971da0462193d53dc3ef6fd4e639d
npcook <nicholas.cook+github@outlook.com>
2016-02-13 15:41:27 -0500

    Change elevator UI prompts to use Main ShowInteractPopup

--- Assets/__Scripts/Scientist.cs
+++ Assets/__Scripts/Scientist.cs
@@ -16,6 +16,10 @@ public class Scientist : MonoBehaviour {
 		if (door != null) {
 			Main.S.ShowInteractPopup(door.gameObject, "Press E to use the hallway");
 		}
+		var elevator = other.GetComponent<ElevatorTrigger>();
+		if (elevator != null) {
+			Main.S.ShowInteractPopup(elevator.gameObject, "Press E to use the elevator");
+		}
 	}
 
 	void OnTriggerStay(Collider other) {
@@ -31,12 +35,28 @@ public class Scientist : MonoBehaviour {
 			usedDoor = true;
 			Invoke("ResetUsedDoor", 0.5f);
 		}
+
+		var elevator = other.GetComponent<ElevatorTrigger>();
+		if (elevator != null && Input.GetAxis("Interact") > 0 && !usedDoor) {
+			Vector3 sciPos = transform.position;
+			Vector3 swarmPos = Swarm.S.transform.position;
+			sciPos.y = elevator.destination.transform.position.y + 0.1f;
+			swarmPos.y = elevator.destination.transform.position.y + 1.1f;
+			transform.position = sciPos;
+			Swarm.S.transform.position = swarmPos;
+			usedDoor = true;
+			Invoke("ResetUsedDoor", 0.5f);
+		}
 	}
 
 	public void OnTriggerExit(Collider other) {
 		var door = other.GetComponentInParent<HallDoor>();
 		if (door != null) {
 			Main.S.HideInteractPopup(door.gameObject);
+		}
+		var elevator = other.GetComponent<ElevatorTrigger>();
+		if (elevator != null) {
+			Main.S.HideInteractPopup(elevator.gameObject);
 		}
 	}
 
