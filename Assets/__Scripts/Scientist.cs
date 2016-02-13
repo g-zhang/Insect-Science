1d95879bcbf895b0153a372a8e78888166b7dd51
npcook <nicholas.cook+github@outlook.com>
2016-02-13 15:05:59 -0500

    Show UI for hallways

--- Assets/__Scripts/Scientist.cs
+++ Assets/__Scripts/Scientist.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 public class Scientist : MonoBehaviour {
 
-    public static Scientist S;
+	public static Scientist S;
 
 	bool usedDoor = false;
 
-	void Awake () {
-	   S = this;
+	void Awake() {
+		S = this;
+	}
+
+	public void OnTriggerEnter(Collider other) {
+		var door = other.GetComponentInParent<HallDoor>();
+		if (door != null) {
+			Main.S.ShowInteractPopup(door.gameObject, "Press E to use the hallway");
+		}
 	}
 
 	void OnTriggerStay(Collider other) {
@@ -18,9 +25,18 @@ public class Scientist : MonoBehaviour {
 		var door = other.GetComponentInParent<HallDoor>();
 		if (door != null && Input.GetAxis("Interact") > 0 && !usedDoor) {
 			var diff = transform.position - other.transform.position;
+			var swarmDiff = Swarm.S.transform.position - transform.position;
 			transform.position = diff + door.linkedDoor.transform.position + new Vector3(0f, 0.1f, 0f);
+			Swarm.S.transform.position = transform.position + swarmDiff;
 			usedDoor = true;
 			Invoke("ResetUsedDoor", 0.5f);
+		}
+	}
+
+	public void OnTriggerExit(Collider other) {
+		var door = other.GetComponentInParent<HallDoor>();
+		if (door != null) {
+			Main.S.HideInteractPopup(door.gameObject);
 		}
 	}
 
