a6bc33f95423bc284ef837145a2e6f17742e297f
npcook <nicholas.cook+github@outlook.com>
2016-02-13 15:06:31 -0500

    Start level selection scene

---
+++ Assets/__Scripts/LevelSelect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour {
+	public void StartTutorial1() {
+	}
+
+	public void StartTutorial2() {
+
+	}
+
+	public void StartLevel1() {
+
+	}
+	
+	public void StartLevel2() {
+
+	}
+}
