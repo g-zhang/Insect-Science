433414b10d0ac1645aa89778fb6b36c0665df7cb
npcook <nicholas.cook+github@outlook.com>
2016-02-15 01:09:43 -0500

    Update level selection

--- Assets/__Scripts/LevelSelect.cs
+++ Assets/__Scripts/LevelSelect.cs
@@ -4,18 +4,34 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelect : MonoBehaviour {
 	public void StartTutorial1() {
-		SceneManager.LoadScene("Scene_Prototype");
+		SceneManager.LoadScene("Scene_Introduction");
 	}
 
 	public void StartTutorial2() {
+		SceneManager.LoadScene("Scene_Prototype");
+	}
+
+	public void StartTutorial3() {
+		SceneManager.LoadScene("Gordon_Tutorial_Level");
+	}
+
+	public void StartTutorial4() {
 		SceneManager.LoadScene("Scene_Tutorial_VentsKeypad");
 	}
 
 	public void StartLevel1() {
-		SceneManager.LoadScene("Scene_Nick");
+		SceneManager.LoadScene("Robbie_Tutorial_Level");
 	}
 	
 	public void StartLevel2() {
-		SceneManager.LoadScene("Robbie_Tutorial_Level");
+		SceneManager.LoadScene("Scene_Nick");
+	}
+
+	public void StartLevel3() {
+		SceneManager.LoadScene("Scene_Nick_2");
+	}
+
+	public void StartLevel4() {
+		SceneManager.LoadScene("Scene_Rob");
 	}
 }
