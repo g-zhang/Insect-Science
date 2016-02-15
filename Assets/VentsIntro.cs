0f3c3247a310ecd785dd58e75970afd4316a11ad
goodingr <goodingr@umich.edu>
2016-02-15 18:11:48 -0500

    added intro scene into Tutorial_VentsKeypad

---
+++ Assets/VentsIntro.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VentsIntro : MonoBehaviour {
+
+	    public GameObject textVents;
+        
+        void OnTriggerEnter(Collider other) {
+            if (other.gameObject.tag != "Player") return;
+         
+            textVents.SetActive(true);
+        }
+}
