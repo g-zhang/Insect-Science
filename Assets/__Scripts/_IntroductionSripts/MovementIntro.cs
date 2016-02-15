488a8b7e33b13da27a2a6ec04f063d88e4da52e2
goodingr <goodingr@umich.edu>
2016-02-14 23:10:28 -0500

    Introduction Scene

---
+++ Assets/__Scripts/_IntroductionSripts/MovementIntro.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementIntro : MonoBehaviour {
+
+	void OnTriggerExit(Collider other) {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/__Scripts/_IntroductionSripts/SwarmIntro.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwarmIntro : MonoBehaviour {
+
+    public GameObject textSplit;
+    public GameObject textSwarmMove;
+    public GameObject keypad;
+
+    private bool completed = false;
+    private bool splitComplete = false;
+
+	void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag != "Player" || splitComplete) return;
+        textSplit.SetActive(true);
+        splitComplete = true;
+    }
+    
+    void OnTriggerStay(Collider other) {
+       if(other.gameObject.tag != "Player") return;
+       float iQ = Input.GetAxis("Split");
+        
+        if (iQ > 0) {
+           textSplit.SetActive(false);
+           textSwarmMove.SetActive(true);
+        }
+        if (!keypad.active)
+        {
+            completed = true;
+        }
+    }
+    
+	void OnTriggerExit(Collider other) {
+        if (completed) {
+            Destroy(gameObject);
+        }
+    }
+}
